4dd2bd0da9ffe9e214e876114e764135fac306f6
jeffbai996 <j.bai@wustl.edu>
2022-11-17 10:27:02 -0800

    Added completed code for Catch Mixed Usage of Single and Double Quotes

--- catch_mixed_usage_of_quotes.js
+++ catch_mixed_usage_of_quotes.js
@@ -15,5 +15,5 @@
 // const allSameQuotes = 'I\'ve had a perfectly wonderful evening, but this wasn\'t it.';
 // Fix the string so it either uses different quotes for the href value, or escape the existing ones. Keep the double quote marks around the entire string.
 
-let innerHtml = "<p>Click here to <a href="#Home">return home</a></p>";
+let innerHtml = "<p>Click here to <a href=\"#Home\">return home</a></p>";
 console.log(innerHtml);
